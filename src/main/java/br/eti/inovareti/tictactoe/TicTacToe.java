95874e0e8df8992a15886a15a837f26df48efbcc
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-23 23:23:20 -0300

    Initial commit

---
+++ src/main/java/br/eti/inovareti/tictactoe/TicTacToe.java
@@ -0,0 +1,9 @@
+package br.eti.inovareti.tictactoe;
+
+import br.eti.inovareti.tictactoe.core.Game;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
---
+++ src/main/java/br/eti/inovareti/tictactoe/core/Game.java
@@ -0,0 +1,10 @@
+package br.eti.inovareti.tictactoe.core;
+
+public class Game {
+
+
+
+    public void play() {
+        System.out.println("Jogando....");
+    }
+}
