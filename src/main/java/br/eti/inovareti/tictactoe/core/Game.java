c92147a11f3958de62470e72387c48a8bd7fa228
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-24 23:00:07 -0300

    Detectando o próximo jogador a jogar

--- src/main/java/br/eti/inovareti/tictactoe/core/Game.java
+++ src/main/java/br/eti/inovareti/tictactoe/core/Game.java
@@ -7,6 +7,7 @@ public class Game {
 
     private Board board = new Board();
     private Player[] players = new Player[Constantes.SYMBOL_PLAYERS.length];
+    private int currentPlayerIndex = 0;
 
     public void play() {
         UI.printGameTitle();
@@ -24,5 +25,19 @@ public class Game {
         UI.printText("O Jogador '" + name + "' vai usar o símbolo '" + symbol + "'.");
 
         return player;
+    }
+
+    private Player nextPlayer() {
+        // Implementação 1
+        /*currentPlayerIndex++;
+        if (currentPlayerIndex >= players.length) {
+            currentPlayerIndex = 0;
+        }
+        return players[currentPlayerIndex];
+        */
+
+        // Implementação 2
+        currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+        return players[currentPlayerIndex];
     }
 }
